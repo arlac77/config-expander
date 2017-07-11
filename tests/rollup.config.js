dee3c9f0b641331fd79d4cb73733ae6b80d15176
Markus Felten <Markus.Felten@gmx.de>
2017-07-11 14:11:27 +0200

    chore: add missing tests/rollup.config.js from template chore(devDependencies): update rollup@^0.45.1 from template chore(devDependencies): add rollup-plugin-commonjs@^8.0.2 from template chore(devDependencies): add rollup-plugin-node-resolve@^3.0.0 from template chore(npm): delete config chore(travis): merge from template .travis.yml chore(git): updated from template

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -3,7 +3,7 @@ import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
   entry: 'tests/**/*-test.js',
-  external: ['ava', 'expression-expander', 'pratt-parser'],
+  external: ['ava'],
   plugins: [
     babel({
       babelrc: false,
