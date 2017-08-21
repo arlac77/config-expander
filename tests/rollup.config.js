ade8c2933c544b222c87e2b53788074750b35024
Markus Felten <Markus.Felten@gmx.de>
2017-08-21 23:53:11 +0200

    chore(rollup): update from template chore(rollup): update from template chore(scripts): update prepare@rollup -c from template chore(devDependencies): update rollup@^0.48.2 from template chore(devDependencies): add rollup-plugin-commonjs@^8.1.0 from template chore(devDependencies): add rollup-plugin-node-resolve@^3.0.0 from template chore(npm): delete config

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  entry: 'tests/**/*-test.js',
+  input: 'tests/**/*-test.js',
   external: ['ava', 'expression-expander', 'pratt-parser'],
   plugins: [
     babel({
