e49a5fc55b534620d4e0836746fc49dadd7e7e0c
Markus Felten <Markus.Felten@gmx.de>
2017-10-15 12:43:32 +0200

    chore(rollup): remove babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
@@ -7,13 +6,6 @@ export default {
       dest: pkg.main,
       format: 'cjs'
     }
-  ],
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      exclude: 'node_modules/**'
-    })
   ],
   external: ['expression-expander', 'pratt-parser']
 };
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,17 +1,9 @@
-import babel from 'rollup-plugin-babel';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
   entry: 'tests/**/*-test.js',
   external: ['ava', 'expression-expander', 'pratt-parser'],
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      exclude: 'node_modules/**'
-    }),
-    multiEntry()
-  ],
+  plugins: [multiEntry()],
   format: 'cjs',
   dest: 'build/test-bundle.js',
   sourceMap: true
