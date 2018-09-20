c84bc64a60e4257546759600ffc6c6ffcba4fd11
Markus Felten <Markus.Felten@gmx.de>
2018-09-20 23:35:10 +0200

    test: ava@1 compatibility

--- tests/files-test.js
+++ tests/files-test.js
@@ -1,8 +1,8 @@
-import test from 'ava';
-import { expand } from '../src/expander';
-import { join } from 'path';
+import test from "ava";
+import { expand } from "../src/expander";
+import { join } from "path";
 
-test('has file content', async t =>
+test("has file content", async t =>
   t.deepEqual(
     Object.values(
       await expand(
@@ -12,61 +12,60 @@ test('has file content', async t =>
         },
         {
           constants: {
-            basedir: join(__dirname, '..', 'tests', 'fixtures')
+            basedir: join(__dirname, "..", "tests", "fixtures")
           }
         }
       )
     ).map(v => v.toString()),
-    ['line 1\n', 'line 1\n']
+    ["line 1\n", "line 1\n"]
   ));
 
-test('has file content', async t =>
+test("has file content #2", async t =>
   t.is(
     await expand("${resolve('fixtures')}", {
       constants: {
-        basedir: join(__dirname, '..', 'tests')
+        basedir: join(__dirname, "..", "tests")
       }
     }),
-    join(__dirname, '..', 'tests', 'fixtures')
+    join(__dirname, "..", "tests", "fixtures")
   ));
 
-test('can include', async t =>
+test("can include", async t =>
   t.deepEqual(
     await expand("${include('../tests/fixtures/other.json')}", {
       constants: {
         basedir: __dirname,
-        c1: 'x'
+        c1: "x"
       }
     }),
     {
-      key: 'value from other x'
+      key: "value from other x"
     }
   ));
 
-test('can nest includes', async t =>
+test("can nest includes", async t =>
   t.deepEqual(
     (await expand("${include('../tests/fixtures/first.json')}", {
       constants: {
-        nameOfTheOther: 'other.json',
+        nameOfTheOther: "other.json",
         basedir: __dirname
       }
     })).first_key,
     {
-      key: 'value from other v1'
+      key: "value from other v1"
     }
   ));
 
-test('include missing', async t => {
-  const error = await t.throws(
-    expand("${include('../tests/fixtures/missing.json')}")
-  );
-  t.is(
-    error.message,
-    `ENOENT: no such file or directory, open '${join(
-      __dirname,
-      '..',
-      '../tests/fixtures/missing.json'
-    )}'`
+test("include missing", async t => {
+  const error = await t.throwsAsync(
+    async () => expand("${include('../tests/fixtures/missing.json')}"),
+    {
+      message: `ENOENT: no such file or directory, open '${join(
+        __dirname,
+        "..",
+        "../tests/fixtures/missing.json"
+      )}'`
+    }
   );
 });
 
