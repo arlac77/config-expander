26dc0e763ca9ae1d68f0595b288b270c59383bf2
Markus Felten <Markus.Felten@gmx.de>
2019-01-02 11:59:32 +0100

    fix(package): expression-expander@7.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,25 @@
 import json from "rollup-plugin-json";
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import cleanup from 'rollup-plugin-cleanup';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import cleanup from "rollup-plugin-cleanup";
+import pkg from "./package.json";
 
 export default {
   output: {
     file: pkg.main,
-    format: 'cjs',
+    format: "cjs",
     interop: false
   },
   plugins: [resolve(), commonjs(), cleanup()],
   external: [
-    'os',
-    'util',
-    'child_process',
-    'path',
-    'fs',
-    'crypto',
-    'expression-expander',
-    'pratt-parser'
+    "os",
+    "util",
+    "child_process",
+    "path",
+    "fs",
+    "crypto",
+    "pratt-parser"
   ],
   input: pkg.module
 };
